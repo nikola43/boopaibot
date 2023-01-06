5ece79dafa0a1255960174446e1da0b8096dbedd
Paulo <pauloxti@gmail.com>
2023-01-06 18:29:32 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,40 @@
+module github.com/nikola43/boopaibot
+
+go 1.19
+
+require (
+	github.com/ethereum/go-ethereum v1.10.26
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	gopkg.in/telebot.v3 v3.1.2
+)
+
+require (
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f // indirect
+	github.com/hrharder/go-gas v1.0.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mdp/qrterminal v1.0.1 // indirect
+	github.com/rjeczalik/notify v0.9.2 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/tklauser/go-sysconf v0.3.10 // indirect
+	github.com/tklauser/numcpus v0.5.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	rsc.io/qr v0.2.0 // indirect
+)
+
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/nikola43/web3golanghelper v0.0.0-20221008144303-4af1a2c4b4f8
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
+)
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/nikola43/web3golanghelper/web3helper"
+	pancakeRouter "buytokenspancakegolang/contracts/IPancakeRouter02"
+)
+
+const (
+	// BotToken is the Telegram Bot API token.
+	//BotToken = "5960863904:AAFdz0O5CwzglyFJ_Hz_nTkJqiZ-zUuBZ_8"
+	BotToken = "5829617229:AAHHqca1eu2PG3FS-bXATVEptV2Y9ky4H-M"
+	// ChatID is the Telegram chat ID.
+	//ChatID = -891755767
+	// Call buybot command
+	CallBuybot = "/buybot"
+	// Call buybot config command
+	CallBuybotConfig = "/buybotconfig"
+)
+
+func main() {
+
+	web3GolangHelper := initWeb3()
+	//migrate(db)
+	routerAddress := "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+	routerAbi, _ := abi.JSON(strings.NewReader(string(pancakeFactory.PancakeABI)))
+
+	bot, err := tgbotapi.NewBotAPI(BotToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message updates
+			continue
+		}
+
+		if !update.Message.IsCommand() { // ignore any non-command Messages
+			continue
+		}
+
+		// Create a new MessageConfig. We don't have text yet,
+		// so we leave it empty.
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		// Extract the command from the Message.
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "I understand /sayhi and /status."
+		case "sayhi":
+			// get the first argument of the command
+			// and use it as the text of the reply
+			msg.Text = "Hi " + update.Message.CommandArguments()
+
+			//msg.Text = update.Message.
+		case "status":
+			msg.Text = "I'm ok."
+		default:
+			msg.Text = "I don't know that command"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
+}
+
+func initWeb3() *web3helper.Web3GolangHelper {
+	pk := "b366406bc0b4883b9b4b3b41117d6c62839174b7d21ec32a5ad0cc76cb3496bd"
+	rpcUrl := "https://speedy-nodes-nyc.moralis.io/84a2745d907034e6d388f8d6/bsc/testnet"
+	wsUrl := "wss://speedy-nodes-nyc.moralis.io/84a2745d907034e6d388f8d6/bsc/testnet/ws"
+	web3GolangHelper := web3helper.NewWeb3GolangHelper(rpcUrl, wsUrl, pk)
+
+	chainID, err := web3GolangHelper.HttpClient().NetworkID(context.Background())
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Chain Id: " + chainID.String())
+	return web3GolangHelper
+}
